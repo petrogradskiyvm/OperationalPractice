1665a490f3e9721b99c846240897cea38e4016ac
Test <petrogradskiyvm@gmail.com>
2026-01-26 21:19:12 +0300

    Добавлено: - виджет для "Ключа" и его обработка

--- GUI.py
+++ GUI.py
@@ -33,3 +33,23 @@ class SosemanukGUI:
             # Настройки шифрования
             settings_frame = ttk.LabelFrame(main_frame, text="Настройки шифрования", padding="10")
             settings_frame.grid(row=0, column=0, sticky=(tk.W, tk.E), pady=(0, 10))
+
+            # Ключ
+            ttk.Label(settings_frame, text="Ключ:").grid(row=0, column=0, sticky=tk.W, pady=5)
+
+            self.key_format = tk.StringVar(value="Hex")
+            key_combo = ttk.Combobox(settings_frame, textvariable=self.key_format,
+                                     values=["Hex", "Текст"], width=8, state="readonly")
+            key_combo.grid(row=0, column=1, sticky=tk.W, padx=(5, 10), pady=5)
+            key_combo.bind('<<ComboboxSelected>>', self.on_format_changed)
+
+            self.key_var = tk.StringVar()
+            self.key_var.trace('w', self.validate_key)
+            key_entry = ttk.Entry(settings_frame, textvariable=self.key_var, width=50)
+            key_entry.grid(row=0, column=2, sticky=(tk.W, tk.E), padx=(0, 10), pady=5)
+
+            ttk.Button(settings_frame, text="Случайный ключ",
+                       command=self.generate_random_key).grid(row=0, column=3, padx=5, pady=5)
+
+            self.key_status = ttk.Label(settings_frame, text="", foreground="red")
+            self.key_status.grid(row=1, column=2, sticky=tk.W, pady=(0, 10))
